c27e3dc27abe15f6771e9529465a0eed1c3a0efc
jseungtaek <82442293+jseungtaek@users.noreply.github.com>
2021-11-04 15:23:05 +0900

    Create BOJ(10845)_큐.cpp

---
+++ BOJ(10845)_큐.cpp
@@ -0,0 +1,61 @@
+//baekjoon 10845 ť
+#include <iostream>
+#include <string.h>
+#include <queue>
+#include <stdio.h>
+using namespace std;
+
+int main(void)
+{
+    queue<int> q;
+    int N, x;
+    char str[6];
+
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%s", str);
+        if (!strcmp(str, "push"))
+        {
+            scanf("%d", &x);
+            q.push(x);
+        }
+        else if (!strcmp(str, "pop"))
+        {
+            if (!q.empty())
+            {
+                printf("%d", q.front());
+                q.pop();
+            }
+            else
+                printf("-1");
+        }
+        else if (!strcmp(str, "size"))
+        {
+            printf("%d", q.size());
+        }
+        else if (!strcmp(str, "empty"))
+        {
+            if (!q.empty())
+                printf("0");
+            else
+                printf("1");
+        }
+        else if (!strcmp(str, "front"))
+        {
+            if (!q.empty())
+                printf("%d", q.front());
+            else
+                printf("-1");
+        }
+        else if (!strcmp(str, "back"))
+        {
+            if (!q.empty())
+                printf("%d", q.back());
+            else
+                printf("-1");
+        }
+        printf("\n");
+    }
+    return 0;
+}
